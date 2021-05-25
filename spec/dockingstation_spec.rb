380cc3aa15afc79d79859444933d16ea9e6b5147
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-05-25 16:45:50 +0100

    Modified spec files

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,7 +1,12 @@
 require 'docking_station.rb'
+require 'bike.rb'
 
 describe DockingStation do
   docking_station = DockingStation.new
-
+  
   it {is_expected.respond_to?(:release_bike)}
+  
+  it do
+    expect(docking_station.release_bike).to eq(true)
+  end
 end
