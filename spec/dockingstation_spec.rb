fe5821c99a3b50419b42f7fa1561172b12fd6bda
Fabio <alessf13@gmail.com>
2021-05-25 15:58:58 +0100

    release_bike method created, and test.

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,9 @@
 require 'docking_station.rb'
 
 describe DockingStation do
+  docking_station = DockingStation.new
 
+  it 'releases a bike' do
+    expect(docking_station).to respond_to(:release_bike)
+  end
 end
