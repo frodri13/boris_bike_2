e88ba447b2dde56cb6bb4dc6f8f3c4a69921ba3a
Fabio <alessf13@gmail.com>
2021-05-25 17:05:46 +0100

    bike.rb content added to docking_station

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,12 +1,9 @@
 require 'docking_station.rb'
-require 'bike.rb'
 
 describe DockingStation do
   docking_station = DockingStation.new
-  
+
   it {is_expected.respond_to?(:release_bike)}
-  
-  it do
-    expect(docking_station.release_bike).to eq(true)
-  end
+
+  it {is_expected.release_bike(:true)}
 end
